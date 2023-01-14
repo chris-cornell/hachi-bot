f28b8c6b9a2f421c6654fbb5a4929110e25fd834
Chris Cornell <cornell@chriss-mbp.mynetworksettings.com>
2023-01-13 21:24:42 -0500

    init

---
+++ src/events/guild-create.ts
@@ -0,0 +1,41 @@
+import {Client, Guild} from 'discord.js';
+import container from '../inversify.config.js';
+import Command from '../commands';
+import {TYPES} from '../types.js';
+import Config from '../services/config.js';
+import {prisma} from '../utils/db.js';
+import {REST} from '@discordjs/rest';
+import {Setting} from '@prisma/client';
+import registerCommandsOnGuild from '../utils/register-commands-on-guild.js';
+
+export async function createGuildSettings(guild: Guild): Promise<Setting> {
+  return prisma.setting.upsert({
+    where: {
+      guildId: guild.id,
+    },
+    create: {
+      guildId: guild.id,
+    },
+    update: {},
+  });
+}
+
+export default async (guild: Guild): Promise<void> => {
+  await createGuildSettings(guild);
+
+  const config = container.get<Config>(TYPES.Config);
+
+  // Setup slash commands
+  if (!config.REGISTER_COMMANDS_ON_BOT) {
+    const client = container.get<Client>(TYPES.Client);
+
+    const rest = new REST({version: '10'}).setToken(config.DISCORD_TOKEN);
+
+    await registerCommandsOnGuild({
+      rest,
+      applicationId: client.user!.id,
+      guildId: guild.id,
+      commands: container.getAll<Command>(TYPES.Command).map(command => command.slashCommand),
+    });
+  }
+};
